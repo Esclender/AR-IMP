25b1cc5bcb05e533d1aadc9d16b7ff5f12884dc0
Esclender <elesclenderlugo@gmail.com>
2024-03-18 14:14:22 -0500

    photos with same px

---
+++ src/js/obtenerS3.js
@@ -0,0 +1,55 @@
+// Import the AWS SDK for JavaScript
+import * as AWS from 'aws-sdk'
+
+// Configure AWS SDK with your credentials
+AWS.config.update({
+  accessKeyId: 'AKIAXGMJMOP3KJOIXTMD',
+  secretAccessKey: '+e+I5ELUmd6U6awp/dqRjIv7wgU/rH2aR9fQ8f80',
+  region: 'sa-east-1'
+});
+
+// Create an S3 instance
+const s3 = new AWS.S3();
+
+// Function to retrieve folders from S3, save them locally in cache, and generate temporary paths
+export default async function retrieveFoldersFromS3AndCache(bucketName, folderPrefix) {
+  try {
+    // List objects in the specified bucket with the given prefix
+    const params = {
+      Bucket: bucketName,
+      Prefix: folderPrefix
+    };
+    const data = await s3.listObjectsV2(params).promise();
+
+    // Extract folder names and file paths from the retrieved objects
+    const folderData = data.Contents.reduce((folders, item) => {
+      const key = item.Key;
+      const [folderName, fileName] = key.split('/'); 
+      if (!folders[folderName]) {
+        folders[folderName] = [];
+      }
+      folders[folderName].push({
+        fileName,
+        s3Path: key,
+        temporaryPath: `temp/${folderName}/` // Generate temporary paths
+      });
+      return folders;
+    }, {});
+
+    // Store folder data in cache
+    localStorage.setItem('cachedFolders', JSON.stringify(folderData));
+    
+    return folderData;
+  } catch (error) {
+    console.error("Error retrieving folders from S3:", error);
+    return {};
+  }
+}
+
+// // Example usage
+// const bucketName = 'mp-example-bucket';
+// const folderPrefix = 'ar-folder/';
+// retrieveFoldersFromS3AndCache(bucketName, folderPrefix)
+//   .then(folderData => {
+//     console.log("Folders retrieved from S3 and cached locally:", folderData);
+//   });
